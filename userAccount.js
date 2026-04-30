02520091a44ad5330f886ef15c8112dfebdc426b
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-30 23:00:22 +0530

    add fnc

--- userAccount.js
+++ userAccount.js
@@ -1,8 +1,13 @@
 logoutBtn = document.getElementById("logoutBtn");
 logoutConfirmBtn = document.getElementById("confirmLogoutBtn");
-logoutContainer =document.getElementsByClassName("logoutContainer")
+logoutContainer = document.getElementsByClassName("logoutContainer")[0];
 
 logoutBtn.addEventListener("click", function() {
   logoutContainer.style.visibility = "visible";
+});
 
+logoutConfirmBtn.addEventListener("click", function() {
+  logoutContainer.style.visibility = "hidden";
+  alert("You have been logged out.");
+  window.location.href = "lgrRn.html";
 });
